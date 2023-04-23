909ca3e3b9152b2a4a0a22394fbd0b39bf85eccd
urt2509 <99096447+urt2509@users.noreply.github.com>
2023-04-23 22:03:44 +0000

    Deploying to gh-pages from @ urt2509/goit-js-hw-09@625d378702fd691b0a5b9c75fb4efbe86a5ed484 🚀

---
+++ 01-color-switcher.7dab9583.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("button");console.log(o);
+//# sourceMappingURL=01-color-switcher.7dab9583.js.map
---
+++ 01-color-switcher.db8faad4.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("button");console.log(o)}();
+//# sourceMappingURL=01-color-switcher.db8faad4.js.map
