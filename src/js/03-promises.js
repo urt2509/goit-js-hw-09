892cbed7d067361bf5d42a8262dbb104453916d4
Alla <urt2509@gmail.com>
2023-05-05 22:18:04 +0200

    task3 updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handlerForm);
+
+/**
+ * Function handles creating promices
+ * @param {event} submit
+ *  */
+function handlerForm(evt) {
+  evt.preventDefault();
+
+  let { delay, step, amount } = evt.currentTarget.elements;
+
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    delay += step;
   }
+  form.reset();
+}
+
+/**
+ * Function creates promices
+ * @param {Number} position
+ * @param {Number} delay
+ * @returns
+ */
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const promiseParam = { position, delay };
+
+      shouldResolve ? resolve(promiseParam) : reject(promiseParam);
+    }, delay);
+  });
 }
